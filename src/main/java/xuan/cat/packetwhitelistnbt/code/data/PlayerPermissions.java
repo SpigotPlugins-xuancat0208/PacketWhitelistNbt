88ce4e26ba2de0cdc8e44131c856da2515f0c5c2
xuancat0208 <xuancat0208@gmail.com>
2022-07-06 06:11:16 +0800

    2.4.1

--- src/main/java/xuan/cat/packetwhitelistnbt/code/data/PlayerPermissions.java
+++ src/main/java/xuan/cat/packetwhitelistnbt/code/data/PlayerPermissions.java
@@ -7,8 +7,6 @@ public final class PlayerPermissions {
     private final Player player;
     /** 檢查權限 */
     private Long permissionsCheck = null;
-    /** 權限命中 */
-    private Integer permissionsHit = null;
     /** 權限需要檢查 */
     public boolean permissionsNeed = true;
     /** 忽略標籤限制 */
@@ -24,7 +22,6 @@ public final class PlayerPermissions {
         if (permissionsNeed || (configData.permissionsPeriodicMillisecondCheck != -1 && (permissionsCheck == null || permissionsCheck <= System.currentTimeMillis() - configData.permissionsPeriodicMillisecondCheck))) {
             permissionsNeed = false;
             permissionsCheck = System.currentTimeMillis();
-            permissionsHit = null;
             // 檢查權限節點
             ignoreItemTagLimit = player.hasPermission(configData.permissionsNode_ignoreItemTagLimit);
         }
